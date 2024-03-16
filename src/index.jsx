1ea56b7553db61b47da0d0c1bc949f20c69b7218
Bacer <dybov.nikita.v@gmail.com>
2024-03-16 12:18:02 +0300

    init project structure with pages, wip textinput with search

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import store from "./services";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
