552e7c2c0736fbd0d33b9af64dfee2850184aa13
timabuev <tim-r10@yandex.ru>
2024-03-17 16:32:47 +0300

    feat: add flexbox to header

--- src/components/app-header-navigation/app-header-navigation.jsx
+++ src/components/app-header-navigation/app-header-navigation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./app-header-avigation.scss";
 
 function AppHeaderNavigation({ children }) {
   return (
--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,27 +1,42 @@
 import { Outlet, NavLink } from "react-router-dom";
 import React from "react";
 import "./app-header.scss";
-import AppHeaderNavigation from "../app-header__navigation/app-header__navigation";
+import AppHeaderNavigation from "../app-header-navigation/app-header-navigation";
 
 function AppHeader() {
   return (
     <>
       <header className="header">
-        <div className="header__logo-HH" />
-        <div className="header__logo-caption" />
-        <AppHeaderNavigation>
-          <NavLink to="/">Главная</NavLink>
-          <NavLink to="/my-requests">Мои заявки</NavLink>
-          <NavLink to="/market-search">Поиск по рынку</NavLink>
-          <NavLink to="/account">Счёт</NavLink>
-          <NavLink to="/help">Помощь</NavLink>
-        </AppHeaderNavigation>
-        <div className="header__heart-icon" />
-        <div className="header__heart-bell" />
-        <div className="header__avatar" />
-        <div className="header__nameSurname">Анастасия Волошина</div>
-        <div className="header__id">#67334</div>
-        <div className="header__profile-container" />
+        <div className="header__container-left">
+          <div className="header__logo-HH" />
+          <div className="header__logo-caption" />
+        </div>
+        <div className="header__container-right">
+          <AppHeaderNavigation>
+            <NavLink to="/" className="link">
+              Главная
+            </NavLink>
+            <NavLink to="/my-requests" className="link">
+              Мои заявки
+            </NavLink>
+            <NavLink to="/market-search" className="link">
+              Поиск по рынку
+            </NavLink>
+            <NavLink to="/account" className="link">
+              Счёт
+            </NavLink>
+            <NavLink to="/help" className="link">
+              Помощь
+            </NavLink>
+          </AppHeaderNavigation>
+          <div className="header__heart-icon" />
+          <div className="header__heart-bell" />
+          <div className="header__avatar" />
+          <div className="header__profile-container">
+            <div className="header__nameSurname">Анастасия Волошина</div>
+            <div className="header__id">#67334</div>
+          </div>
+        </div>
       </header>
       <Outlet />
     </>
