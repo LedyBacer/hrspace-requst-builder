2b8148d58109d809f2f57d24e0c953407af388e3
timabuev <tim-r10@yandex.ru>
2024-03-17 13:44:47 +0300

    feat: add npm install --save prop-types

--- src/components/app-header__navigation/app-header__navigation.jsx
+++ src/components/app-header__navigation/app-header__navigation.jsx
@@ -1,14 +1,19 @@
 // import { NavLink } from "react-router-dom";
 import React from "react";
+import PropTypes from "prop-types";
 
-function AppHeaderNavigation(props) {
+function AppHeaderNavigation({ children }) {
   return (
     <nav className="appHeaderNavigation__navigation">
       <ul className="appHeaderNavigation__nav-ul">
-        <li className="appHeaderNavigation__nav-li">{props.children}</li>
+        <li className="appHeaderNavigation__nav-li">{children}</li>
       </ul>
     </nav>
   );
 }
+
+AppHeaderNavigation.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default AppHeaderNavigation;
