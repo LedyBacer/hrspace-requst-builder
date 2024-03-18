a9494e9e3ca3f6160f8ae2d576e2a346dacab47c
timabuev <tim-r10@yandex.ru>
2024-03-18 13:03:09 +0300

    feat: add images

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,18 @@ import NotFound404 from "./pages/not-found/not-found";
 import AppHeader from "./components/app-header/app-header";
 import HomePage from "./pages/home-page/home-page";
 import RequestBuilder from "./pages/request-builder/request-builder";
-import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <Routes>
       <Route element={<AppHeader />}>
-        <Route element={<Footer />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/request-builder" element={<RequestBuilder />} />
-          {/* <Route path="/request-builder/2" element={}/> */}
-          {/* <Route path="/request-builder/3" element={}/> */}
-          {/* <Route path="/pay" element={}/> */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/request-builder" element={<RequestBuilder />} />
+        {/* <Route path="/request-builder/2" element={}/> */}
+        {/* <Route path="/request-builder/3" element={}/> */}
+        {/* <Route path="/pay" element={}/> */}
 
-          <Route path="*" element={<NotFound404 />} />
-        </Route>
+        <Route path="*" element={<NotFound404 />} />
       </Route>
     </Routes>
   );
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,8 +4,13 @@ import "./footer.scss";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__logo" />
-      <p className="footer__text">© 2022 Группа компаний HeadHunter</p>
+      <div className="footer__left-container">
+        <div className="footer__logo" />
+        <p className="footer__text">© 2022 Группа компаний HeadHunter</p>
+      </div>
+      <div className="fooer__right-container">
+        <div className="footer__links-container" />
+      </div>
     </footer>
   );
 }
