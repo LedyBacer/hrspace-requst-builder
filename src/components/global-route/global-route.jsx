7b7d8495260fd6bc6133929ec4368c5835ad18a5
Bacer <dybov.nikita.v@gmail.com>
2024-03-18 13:15:09 +0300

    create base structure global-route, request-builder

--- src/components/global-route/global-route.jsx
+++ src/components/global-route/global-route.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import styles from "./global-route.module.scss";
 import AppHeader from "../app-header/app-header";
+import Footer from "../footer/footer";
 
 function GlobalRoute() {
   return (
     <div className={styles.container}>
       <AppHeader />
       <Outlet />
-      {/* <AppFooter /> */}
+      <Footer />
     </div>
   );
 }
