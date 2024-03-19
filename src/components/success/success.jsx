45eb0717730a5f7270a01caaa02d545f0eb2f75e
timabuev <tim-r10@yandex.ru>
2024-03-19 17:14:38 +0300

    feat: add pic && text to successWindow

--- src/components/success/success.jsx
+++ src/components/success/success.jsx
@@ -1,7 +1,23 @@
+/* eslint-disable no-irregular-whitespace */
 import React from "react";
+import styles from "./success.module.scss";
 
 function Success() {
-  return <p>success window</p>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.successPicture} />
+      <div className={styles.containerForText}>
+        <h2 className={styles.h2}>Заявка оформлена!</h2>
+        <p className={styles.paragraph}>
+          Сумма поступила на ваш счёт и будет заморожена до закрытия заявки.{" "}
+        </p>
+        <p className={styles.paragraph}>
+          Ваша заявка отправлена на модерацию. Вы можете отслеживать её статус
+          в разделе «Мои заявки».
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Success;
