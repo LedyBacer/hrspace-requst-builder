eda52f69b8ccb54a3ce41c49f5843e171791d940
timabuev <tim-r10@yandex.ru>
2024-03-20 10:50:31 +0300

    feat: add pic & add margin & change captions

--- src/components/404-window/404-window.jsx
+++ src/components/404-window/404-window.jsx
@@ -8,11 +8,7 @@ function Window404() {
     <div className={styles.container}>
       <div className={styles.successPicture} />
       <div className={styles.containerForText}>
-        <h2 className={styles.h2}>Не удалось оформить заявку</h2>
-        <p className={styles.paragraph}>
-          К сожалению, оплата не прошла. Повторите попытку или обратитесь
-          в техподдержку.
-        </p>
+        <h2 className={styles.h2}>Такой страницы нет</h2>
       </div>
       <div className={styles.containerForButtons}>
         <div className={styles.containerForBtn}>
@@ -26,22 +22,7 @@ function Window404() {
               textTransform: "none",
             }}
           >
-            <p className={styles.button_text}>Поддержка</p>
-          </Button>
-        </div>
-
-        <div className={styles.containerForBtn}>
-          <Button
-            variant="contained"
-            color="rqback"
-            sx={{
-              height: "46px",
-              width: "180px",
-              borderRadius: "8px",
-              textTransform: "none",
-            }}
-          >
-            <p className={styles.button_text}>К оплате</p>
+            <p className={styles.button_text}>На главную</p>
           </Button>
         </div>
       </div>
