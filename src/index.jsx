2d353255b2073c3d464e636185d1ed3d07c0efe7
Bacer <dybov.nikita.v@gmail.com>
2024-03-20 22:35:15 +0300

    fix paths

--- src/index.jsx
+++ src/index.jsx
@@ -16,7 +16,7 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/hrspace-requst-builder">
+      <BrowserRouter basename="/hrspace-request-builder-frontend">
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
